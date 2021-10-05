f1bc277dfd6eb287306b67a143db9a3fc7ad1940
leonsanbo <kory.zhou@gmail.com>
2021-10-05 00:17:23 -0700

    block update

--- dgemm-blocked-omp.cpp
+++ dgemm-blocked-omp.cpp
@@ -33,7 +33,7 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
    // #pragma omp parallel
    // {
       LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
-      // #pragma omp for 
+      #pragma omp parallel for collapse(2) 
       for (int i = 0; i < nblocks; i++){
          for (int j = 0; j < nblocks; j++){ 
             //copy from C[i*bs, j*bs] into Clocal
